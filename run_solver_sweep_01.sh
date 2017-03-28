d07437585d0de0456e3fe2ebbb36f59e03c9ae23
Stephen Wood <swood19@utk.edu>
2017-03-28 19:06:19 -0400

    simple run script for parameter sweeps

---
+++ run_solver_sweep_01.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+RUNNAME="MKLFGMRES"
+REPETITION="01"
+MACHINE="beacon"
+DATE=$(date +'%Y%m%d')
+DIR=log_${RUNNAME}_${MACHINE}_${DATE}_${REPETITION}
+#threads=(1 16 32)
+threads=(1 68 136 204 272)
+#threads=(1 `seq 2 2 272`)
+#threads=(1 `seq 2 2 60`)
+precommand=""
+
+
+MATDIR="testing/matrices/"
+MATEXT=".mtx"
+MATRIX="30p30n"
+RHS="30p30n-b"
+OUTDIR="out_test/"
+#SCALING=(NONE UNITDIAG)
+SCALING=(NONE)
+#GMRESRELTOL=(1.0e-3 1.0e-6)
+GMRESRELTOL=(1.0e-3)
+#GMRESRESTART=(100 1000 2000)
+GMRESRESTART=(2000)
+#GMRESMAXITER=(100 1000 2000)
+GMRESMAXITER=(2000)
+PRECONDCHOICE=(0)
+REDUCTION=(1.0e-1 1.0e-3 1.0e-10 1.0e-15)
+
+MATRIXFILE=${MATDIR}${MATRIX}${MATEXT}
+RHSFILE=${MATDIR}${RHS}${MATEXT}
+
+mkdir -p ${DIR}
+for thread in ${threads[@]}; do
+  export OMP_NUM_THREADS=${thread}
+  echo OMP_NUM_THREADS = $OMP_NUM_THREADS
+  for scale in ${SCALING[@]}; do
+    echo SCALING = ${scale}
+    for restart in ${GMRESRESTART[@]}; do
+      echo GMRESRESTART = ${restart}
+      for maxiter in ${GMRESMAXITER[@]}; do
+        echo GMRESMAXITER = ${maxiter}
+        for gtol in ${GMRESRELTOL[@]}; do
+          echo GMRESRELTOL = ${gtol}
+          for ptol in ${REDUCTION[@]}; do
+            echo REDUCTION = ${ptol}
+            LOGFILE=${OUTDIR}log_${MATRIX}_${RHS}_${thread}_${restart}_${maxiter}_${gtol}_ParilU0v02_${ptol}.m
+            echo writing to ${LOGFILE}
+            ${precommand} ./test_solve_pariLU0_MKL_FGMRES \
+            ${MATRIXFILE} ${RHSFILE} ${OUTDIR} ${scale} ${gtol} ${restart} \
+            ${maxiter} ${PRECONDCHOICE} ${ptol} > ${LOGFILE} 2>&1
+          done
+        done
+      done
+    done
+  done
+done
