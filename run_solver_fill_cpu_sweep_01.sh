75312a3a13fc154b3cf592ff4a57f76387c12f1b
Stephen Wood <swood@beacon-login2.nics.utk.edu>
2017-03-29 12:39:24 -0400

    run scripts from beacon for cpu with and without fill

--- run_solver_fill_cpu_sweep_01.sh
+++ run_solver_fill_cpu_sweep_01.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-RUNNAME="MKLFGMRES"
+RUNNAME="MKLFGMRES_fill"
 REPETITION="01"
 MACHINE="beacon"
 DATE=$(date +'%Y%m%d')
 LOGDIR=log_${RUNNAME}_${MACHINE}_${DATE}_${REPETITION}
-#threads=(1 16 32)
-threads=(1 68 136 204 272)
+threads=(1 16 32)
+#threads=(1 68 136 204 272)
 #threads=(1 `seq 2 2 272`)
 #threads=(1 `seq 2 2 60`)
 #precommand=""
@@ -14,7 +14,7 @@ precommand="numactl --interleave=all"
 
 MATDIR="testing/matrices/"
 MATEXT=".mtx"
-MATRIX="30p30n"
+MATRIXLIST=(30p30n-A1 30p30n-A2 30p30n-A3)
 RHS="30p30n-b"
 #OUTDIR="out_test/"
 #SCALING=(NONE UNITDIAG)
@@ -28,11 +28,13 @@ GMRESMAXITER=(2000)
 PRECONDCHOICE=(0)
 REDUCTION=(1.0e-1 1.0e-3 1.0e-10 1.0e-15)
 
-MATRIXFILE=${MATDIR}${MATRIX}${MATEXT}
 RHSFILE=${MATDIR}${RHS}${MATEXT}
 
 mkdir -p ${LOGDIR}
 #mkdir -p ${OUTDIR}
+for matrix in ${MATRIXLIST[@]}; do
+MATRIXFILE=${MATDIR}${matrix}${MATEXT}
+
 for thread in ${threads[@]}; do
   export OMP_NUM_THREADS=${thread}
   echo OMP_NUM_THREADS = $OMP_NUM_THREADS
@@ -46,9 +48,9 @@ for thread in ${threads[@]}; do
           echo GMRESRELTOL = ${gtol}
           for ptol in ${REDUCTION[@]}; do
             echo REDUCTION = ${ptol}
-            LOGFILE=${LOGDIR}/log_${MATRIX}_${RHS}_${thread}_${restart}_${maxiter}_${gtol}_ParilU0v02_${ptol}.m
+            LOGFILE=${LOGDIR}/log_${matrix}_${RHS}_${thread}_${scale}_${restart}_${maxiter}_${gtol}_ParilU0v02_${ptol}.m
             echo writing to ${LOGFILE}
-            ${precommand} ./test_solve_pariLU0_MKL_FGMRES_knl \
+            ${precommand} ./test_solve_pariLU0_MKL_FGMRES \
             ${MATRIXFILE} ${RHSFILE} ${LOGDIR} ${scale} ${gtol} ${restart} \
             ${maxiter} ${PRECONDCHOICE} ${ptol} > ${LOGFILE} 2>&1
           done
@@ -59,8 +61,23 @@ for thread in ${threads[@]}; do
 done
 
 PRECONDCHOICE=(1)
-LOGFILE=${LOGDIR}/log_${MATRIX}_${RHS}_${thread}_${restart}_${maxiter}_${gtol}_MKLcsriLU0_${ptol}.m
-echo writing to ${LOGFILE}
-${precommand} ./test_solve_pariLU0_MKL_FGMRES_knl \
-${MATRIXFILE} ${RHSFILE} ${LOGDIR} ${scale} ${gtol} ${restart} \
-${maxiter} ${PRECONDCHOICE} ${ptol} > ${LOGFILE} 2>&1
+
+for scale in ${SCALING[@]}; do
+  echo SCALING = ${scale}
+  for restart in ${GMRESRESTART[@]}; do
+    echo GMRESRESTART = ${restart}
+    for maxiter in ${GMRESMAXITER[@]}; do
+      echo GMRESMAXITER = ${maxiter}
+      for gtol in ${GMRESRELTOL[@]}; do
+        echo GMRESRELTOL = ${gtol}
+        LOGFILE=${LOGDIR}/log_${matrix}_${RHS}_${thread}_${scale}_${restart}_${maxiter}_${gtol}_MKLcsriLU0_${ptol}.m
+        echo writing to ${LOGFILE}
+        ${precommand} ./test_solve_pariLU0_MKL_FGMRES \
+        ${MATRIXFILE} ${RHSFILE} ${LOGDIR} ${scale} ${gtol} ${restart} \
+        ${maxiter} ${PRECONDCHOICE} ${ptol} > ${LOGFILE} 2>&1
+      done 
+    done
+  done 
+done
+
+done
