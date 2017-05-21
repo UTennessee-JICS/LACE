9b56395ed8182cc508a85b45c9e48c48f4dd3857
Stephen Wood <swood19@utk.edu>
2017-05-20 21:58:18 -0400

    use the LU factorization of a random dense matrix to test trisolves

--- test_dense_trisolve.cpp
+++ test_dense_trisolve.cpp
@@ -130,26 +130,17 @@ int main(int argc, char* argv[])
   for ( int i = 0; i<A.num_rows; i++ ) {
     for ( int j = 0; j<A.num_cols; j++ ) {
       if (i == j) {
-        A.val[ i*A.ld + j ] += 1.0e3; 
+        A.val[ i*A.ld + j ] = 1.0e2; 
       }
     }
   }
+  
   //data_zdisplay_dense( &A );
   data_d_matrix B = {Magma_DENSE};
   data_zmconvert( A, &B, Magma_DENSE, Magma_DENSE );
-  
+	
   DEV_CHECKPT
   
-  data_d_matrix L = {Magma_DENSEL};
-  L.diagorder_type = Magma_UNITY;
-  L.fill_mode = MagmaLower;
-  L.major = MagmaRowMajor;
-  
-  data_zmconvert( A, &L, Magma_DENSE, Magma_DENSEL );
-  
-	data_d_matrix rhs_vector = {Magma_DENSE};
-	rhs_vector.major = MagmaRowMajor;
-	
 	// setup expected answer
 	data_d_matrix y_expect = {Magma_DENSE};
   y_expect.num_rows = A.num_rows;
@@ -165,10 +156,9 @@ int main(int argc, char* argv[])
       y_expect.val[i] = 1.0;
     }
   }
-	
-  DEV_CHECKPT
   
-  // Setup rhs for L
+  // Setup rhs 
+  data_d_matrix rhs_vector = {Magma_DENSE};
   rhs_vector.num_rows = A.num_rows;
   rhs_vector.num_cols = 1;
   rhs_vector.ld = 1;
@@ -183,8 +173,24 @@ int main(int argc, char* argv[])
   dataType one = 1.0;
   dataType zero = 0.0;
   dataType negone = -1.0;
-  cblas_dgemv( cblas_order_const(L.major), cblas_trans_const(trans), L.num_rows, L.num_cols,
-    one, L.val, L.ld, y_expect.val, 1, zero, rhs_vector.val, 1 );
+  cblas_dgemv( cblas_order_const(A.major), cblas_trans_const(trans), A.num_rows, A.num_cols,
+    one, A.val, A.ld, y_expect.val, 1, zero, rhs_vector.val, 1 );
+  
+ 
+  DEV_CHECKPT
+  
+  // parLU
+  data_d_matrix L = {Magma_DENSE};
+  data_d_matrix U = {Magma_DENSE};
+  L.diagorder_type = Magma_UNITY;
+  L.fill_mode = MagmaLower;
+  U.diagorder_type = Magma_VALUE;
+  U.fill_mode = MagmaUpper;
+  data_ParLU_v1_0(&A, &L, &U);
+  printf("L:\n");
+  data_zdisplay_dense( &L );
+  printf("U:\n");
+  data_zdisplay_dense( &U );
 
   DEV_CHECKPT
   
@@ -205,29 +211,29 @@ int main(int argc, char* argv[])
 
 	parwcsrtrsvtime = wcsrtrsvend - wcsrtrsvstart;
 	
-  dataType error = 0.0;
-  data_norm_diff_vec( &y, &y_expect, &error );
-  printf("ptrsv_tol = %e ptrsv_iter = %d\n", ptrsv_tol, ptrsv_iter );
-  printf("y error = %.16e\n", error);
-  
-  DEV_CHECKPT
-
-  dataType* Ay;
-  //LACE_CALLOC(Ay, L.num_rows);
-  Ay = (dataType*) calloc( (L.num_rows), sizeof(dataType) );
-  cblas_dgemv( cblas_order_const(L.major), cblas_trans_const(trans), L.num_rows, L.num_cols,
-    one, L.val, L.ld, y.val, 1, zero, Ay, 1 );
-  cblas_daxpy(L.num_rows, negone, rhs_vector.val, 1, Ay, 1 );
-  
-  error = dataType(0.0);
-  for (int i=0; i<L.num_rows; i++) {
-    //printf("Ay[%d] = %e\n", i, Ay[i]);
-    error += pow(Ay[i], 2);
-  }
-  error = sqrt(error);
-  printf("system errors:\n\terror = %.16e\n", error);
-  printf("Par time : %e\n", 
-    parwcsrtrsvtime); 
+  //dataType error = 0.0;
+  //data_norm_diff_vec( &y, &y_expect, &error );
+  //printf("ptrsv_tol = %e ptrsv_iter = %d\n", ptrsv_tol, ptrsv_iter );
+  //printf("y error = %.16e\n", error);
+  //
+  //DEV_CHECKPT
+  //
+  //dataType* Ay;
+  ////LACE_CALLOC(Ay, L.num_rows);
+  //Ay = (dataType*) calloc( (L.num_rows), sizeof(dataType) );
+  //cblas_dgemv( cblas_order_const(L.major), cblas_trans_const(trans), L.num_rows, L.num_cols,
+  //  one, L.val, L.ld, y.val, 1, zero, Ay, 1 );
+  //cblas_daxpy(L.num_rows, negone, rhs_vector.val, 1, Ay, 1 );
+  //
+  //error = dataType(0.0);
+  //for (int i=0; i<L.num_rows; i++) {
+  //  //printf("Ay[%d] = %e\n", i, Ay[i]);
+  //  error += pow(Ay[i], 2);
+  //}
+  //error = sqrt(error);
+  //printf("system errors:\n\terror = %.16e\n", error);
+  //printf("Par time : %e\n", 
+  //  parwcsrtrsvtime); 
   
   // improve y with error and solve again
   //printf("\n===============================================================\n");
@@ -262,26 +268,26 @@ int main(int argc, char* argv[])
   //}
   //error = sqrt(error);
   //printf("system errors:\n\terror = %e\n", error);
-  //printf("Par time : %e\n", 
-  //  parwcsrtrsvtime); 
+  printf("Par time : %e\n", 
+    parwcsrtrsvtime); 
  
   getchar();
 
 //------------------------------------------------------------------------------  
   
-  data_d_matrix U = {Magma_DENSEU};
-  //U.diagorder_type = Magma_VALUE;
-  U.diagorder_type = Magma_UNITY;
-  U.fill_mode = MagmaUpper;
-  U.major = MagmaRowMajor;
-  
-  data_zmconvert( A, &U, Magma_DENSE, Magma_DENSEU );
-  
-  // Setup rhs for U
-  cblas_dgemv( cblas_order_const(U.major), cblas_trans_const(trans), U.num_rows, U.num_cols,
-    one, U.val, U.ld, y_expect.val, 1, zero, rhs_vector.val, 1 );
-  
-  DEV_CHECKPT
+  //data_d_matrix U = {Magma_DENSEU};
+  ////U.diagorder_type = Magma_VALUE;
+  //U.diagorder_type = Magma_UNITY;
+  //U.fill_mode = MagmaUpper;
+  //U.major = MagmaRowMajor;
+  //
+  //data_zmconvert( A, &U, Magma_DENSE, Magma_DENSEU );
+  //
+  //// Setup rhs for U
+  //cblas_dgemv( cblas_order_const(U.major), cblas_trans_const(trans), U.num_rows, U.num_cols,
+  //  one, U.val, U.ld, y_expect.val, 1, zero, rhs_vector.val, 1 );
+  //
+  //DEV_CHECKPT
 
   data_d_matrix x = {Magma_DENSE};
   x.num_rows = A.num_rows;
@@ -294,33 +300,36 @@ int main(int argc, char* argv[])
   
   wcsrtrsvstart = omp_get_wtime();
   data_partrsv_dot( MagmaRowMajor, MagmaUpper, Magma_DENSEU, Magma_VALUE,
-    U.num_rows, U.val, U.ld, rhs_vector.val, 1, x.val, 1, 
+    U.num_rows, U.val, U.ld, y.val, 1, x.val, 1, 
     ptrsv_tol, &ptrsv_iter );
   wcsrtrsvend = omp_get_wtime();
 
 	parwcsrtrsvtime = wcsrtrsvend - wcsrtrsvstart;
 	
-  error = 0.0;
+  dataType error = 0.0;
   data_norm_diff_vec( &x, &y_expect, &error );
   printf("ptrsv_tol = %e ptrsv_iter = %d\n", ptrsv_tol, ptrsv_iter );
-  printf("x error = %e\n", error);
+  printf("x error = %e, num_rows = %d, x error/num_rows = %e\n", 
+    error, A.num_rows, error/((dataType)A.num_rows));
   
   DEV_CHECKPT
 
   dataType* Ax;
   //LACE_CALLOC(Ax, U.num_rows);
   Ax = (dataType*) calloc( (U.num_rows), sizeof(dataType) );
-  cblas_dgemv( cblas_order_const(L.major), cblas_trans_const(trans), U.num_rows, U.num_cols,
-    one, U.val, U.ld, x.val, 1, zero, Ax, 1 );
+  cblas_dgemv( cblas_order_const(L.major), cblas_trans_const(trans), A.num_rows, A.num_cols,
+    one, A.val, A.ld, x.val, 1, zero, Ax, 1 );
   cblas_daxpy(U.num_rows, negone, rhs_vector.val, 1, Ax, 1 );
   
   error = dataType(0.0);
-  for (int i=0; i<L.num_rows; i++) {
+  for (int i=0; i<U.num_rows; i++) {
     printf("Ax[%d] = %e\n", i, Ax[i]);
     error += pow(Ax[i], 2);
   }
   error = sqrt(error);
-  printf("system errors:\n\terror = %e\n", error);
+  //printf("system errors:\n\terror = %e\n", error);
+  printf("system error = %e, num_rows = %d, system error/num_rows = %e\n", 
+    error, A.num_rows, error/((dataType)A.num_rows));
   printf("Par time : %e\n", 
     parwcsrtrsvtime); 
   
@@ -341,7 +350,7 @@ int main(int argc, char* argv[])
   data_zmfree( &L );
   data_zmfree( &U );
 
-  free( Ay );
+  //free( Ay );
   free( Ax );
 
   
