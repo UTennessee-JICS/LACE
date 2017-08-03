6df23f43535de393d87768bd9db1c797524fe862
Stephen Wood <swood19@utk.edu>
2017-08-03 18:56:34 -0400

    initial Householder Transformation implementation and unit test

---
+++ src/householder.cpp
@@ -0,0 +1,160 @@
+/*
+    -- LACE (version 0.0) --
+       Univ. of Tennessee, Knoxville
+       
+       @author Stephen Wood
+
+*/
+#include "../include/sparse.h"
+#include <mkl.h>
+#include <float.h> 
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+    Purpose
+    -------
+
+    Generate Householder reflection.
+    G. W. Stewart, "Matrix Algorithms, Volume 1", SIAM, 1998.
+    [u,nu] = housegen(x).
+    H = I - uu' with Hx = -+ nu e_1
+    nu = norm(x).  
+
+    Arguments
+    ---------
+
+    @param[in]
+    n           int
+                number of elements in X
+
+    @param[in,out]
+    X           dataType *
+                vector to be reflected
+
+    @param[in,out]
+    u           dataType *
+                reflected vector
+
+    @param[in]
+    nu          dataType *
+                norm of X
+
+    @ingroup datasparse_orthogonality
+    ********************************************************************/
+
+
+extern "C" 
+void
+data_housegen( int n,
+  dataType* X,  
+  dataType* u,
+  dataType* nu ) 
+{
+  
+  dataType eps = nextafter(0.0,1.0);
+    
+  //LACE_CALLOC( u, n );
+  for (int i=0; i<n; i++) {
+    u[i] = X[i];
+  }
+  
+  *nu = data_dnrm2( n, X, 1 );
+  if ( (*nu) < eps ) {
+    u[0] = sqrt(2);
+    return;
+  }
+  for (int i=0; i<n; i++) {
+    u[i] = X[i]/(*nu);
+  }
+  if ( u[0] >= 0 ) {
+     u[0] = u[0] + 1.0;
+     (*nu) = -(*nu);
+  }
+  else {
+    u[0] = u[0] - 1.0; 
+  }
+  dataType tmp = 1.0/sqrt(fabs(u[0]));
+  for (int i=0; i<n; i++) {
+    u[i] = u[i]*tmp;
+  }
+  
+}
+
+
+/**
+    Purpose
+    -------
+
+    Householder triangularization.  [U,R] = hqrd(X);
+    Generators of Householder reflections stored in U->
+    H_k = I - U(:,k)*U(:,k)'.
+    prod(H_m ... H_1)X = [R; 0]
+    where m = min(size(X))
+    G. W. Stewart, "Matrix Algorithms, Volume 1", SIAM, 1998.
+
+    Arguments
+    ---------
+
+    @param[in]
+    n           int
+                number of rows elements in X
+
+    @param[in,out]
+    X           dataType *
+                vector to be reflected
+
+    @param[in,out]
+    u           dataType *
+                reflected vector
+
+    @param[in]
+    nu          dataType *
+                norm of X
+
+    @ingroup datasparse_orthogonality
+    ********************************************************************/
+
+
+extern "C" 
+void
+data_hqrd( data_d_matrix* X,  
+  data_d_matrix* U,
+  data_d_matrix* R ) 
+{
+  printf("data_hqrd begin\n X\n");
+  data_zdisplay_dense( X );
+  
+  int length = 0;
+  int n = X->num_rows;
+  int p = X->num_cols;
+  dataType* v;
+  LACE_CALLOC( v, n );
+  for ( int k=0; k<MIN(n,p); k++ ) {
+    length = n - k;
+    data_housegen( length, &(X->val[idx(k,k,X->ld)]), &(U->val[idx(k,k,U->ld)]), &(R->val[idx(k,k,R->ld)]) );
+    for ( int j=k+1; j<p; j++ ) {
+      dataType colsum = 0.0;
+      for ( int i=k; i<n; i++ ) {
+        printf("U[%d,%d] = %e X[%d,%d] = %e\n", i, k, U->val[idx(i,k,U->ld)], i, j, X->val[idx(i,j,X->ld)] );
+        colsum = colsum + U->val[idx(i,k,U->ld)] * X->val[idx(i,j,X->ld)];
+      }
+      v[j] = colsum;
+      printf("k=%d v[%d] = %e\n", k, j, v[j] );
+    }
+    for ( int i=k; i<n; i++ ) {
+      length = n - k;
+      for ( int j=k+1; j<p; j++ ) {
+        X->val[idx(i,j,X->ld)] = X->val[idx(i,j,X->ld)] - U->val[idx(i,k,U->ld)] * v[j];
+        //R->val[idx(i,j,X->ld)] = X->val[idx(i,j,X->ld)];
+      }
+    }
+    for ( int j=k+1; j<p; j++ ) {
+      printf("R[%d,%d] = %e\n", k, j, R->val[idx(k,j,X->ld)] );
+      //X->val[idx(i,j,X->ld)] = X->val[idx(i,j,X->ld)] - U->val[idx(i,k,U->ld)] * v[j];
+      R->val[idx(k,j,X->ld)] = X->val[idx(k,j,X->ld)];
+    }
+    
+  }
+  
+}
---
+++ test_householder.cpp
@@ -0,0 +1,145 @@
+/*
+
+*/
+
+#include "include/sparse.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <string.h>
+#include <vector>
+#include <omp.h>
+#include <float.h> 
+#include "math.h"
+
+#define size 128
+
+//+++++++
+#define EXPECT_ITERABLE_DOUBLE_EQ( TYPE, ref, target) \
+{ \
+const TYPE& _ref(ref); \
+const TYPE& _target(target); \
+TYPE::const_iterator tarIter   = _target.begin(); \
+TYPE::const_iterator refIter = _ref.begin(); \
+unsigned int i = 0; \
+while(refIter != _ref.end()) { \
+    if ( tarIter == _target.end() ) { \
+        ADD_FAILURE() << #target \
+            " has a smaller length than " #ref ; \
+        break; \
+    } \
+    EXPECT_DOUBLE_EQ(* refIter, * tarIter) \
+        << "Vectors " #ref  " (refIter) " \
+           "and " #target " (tarIter) " \
+           "differ at index " << i; \
+    ++refIter; ++tarIter; ++i; \
+} \
+EXPECT_TRUE( tarIter == _target.end() ) \
+    << #ref " has a smaller length than " \
+       #target ; \
+}
+//+++++++
+
+//+++++++
+#define EXPECT_ARRAY_DOUBLE_EQ( length, ref, target) \
+{ \
+  unsigned int i = 0; \
+  for(i=0; i<length; i++) { \
+    EXPECT_DOUBLE_EQ(ref[i], target[i]) \
+    << "Arrays " #ref  " and " #target \
+           "differ at index " << i; \
+  } \
+}
+//+++++++ 
+
+//+++++++
+#define EXPECT_ARRAY_INT_EQ( length, ref, target) \
+{ \
+  unsigned int i = 0; \
+  for(i=0; i<length; i++) { \
+    EXPECT_EQ(ref[i], target[i]) \
+    << "Arrays " #ref  " and " #target \
+           "differ at index " << i; \
+  } \
+}
+//+++++++ 
+
+
+int main(int argc, char* argv[])
+{
+  
+  // begin with a square matrix A
+  char* sparse_filename;
+  char* rhs_filename;
+  char* sparse_basename;
+  char sparse_name[256];
+  char* output_dir;
+  char output_basename[256];
+  
+  dataType zero = 0.0;
+  dataType one = 1.0;
+  dataType negone = -1.0;
+  
+  if (argc < 2) {
+    printf("Usage %s <matrix> \n", argv[0] );
+    return 1;
+  }
+  else {
+    sparse_filename = argv[1];
+    sparse_basename = basename( sparse_filename );
+    char *ext;
+    ext = strrchr( sparse_basename, '.');
+    strncpy( sparse_name, sparse_basename, int(ext - sparse_basename) );
+    printf( "File %s basename %s name %s \n", 
+      sparse_filename, sparse_basename, sparse_name );
+  }
+	data_d_matrix Adense = {Magma_DENSE};
+  CHECK( data_z_dense_mtx( &Adense, MagmaColMajor, sparse_filename ) );
+	
+  DEV_CHECKPT
+  printf( "Adense num_rows = %d num_cols = %d ld =%d\n",
+    Adense.num_rows, Adense.num_cols, Adense.ld );
+  data_zdisplay_dense( &Adense );
+  
+  dataType* v;
+  LACE_CALLOC( v, Adense.num_rows );
+  dataType nu = 0;
+  data_housegen( Adense.num_rows, Adense.val, v, &nu );
+  for ( int i=0; i<Adense.num_rows; i++ ) {
+    printf( "Adense.val[%d] = %e\n", i, Adense.val[idx(i,0,Adense.ld)] ); 
+  }
+  for ( int i=0; i<Adense.num_rows; i++ ) {
+    printf( "v.val[%d] = %e\n", i, v[i] ); 
+  }
+  printf( "nu =%e\n", nu );
+  
+  data_d_matrix U = {Magma_DENSE};
+  data_zvinit( &U, Adense.num_rows, Adense.num_cols, zero );
+  int m = MIN( Adense.num_rows, Adense.num_cols );
+  data_d_matrix R = {Magma_DENSE};
+  data_zvinit( &R, m, m, zero );
+  
+  data_hqrd( &Adense, &U, &R );
+  
+  
+  printf("Adense\n");
+  data_zdisplay_dense( &Adense );
+  printf("U\n");
+  data_zdisplay_dense( &U );
+  printf("R\n");
+  data_zdisplay_dense( &R );
+  
+  data_zmfree( &Adense );
+	data_zmfree( &U );
+  data_zmfree( &R );
+  
+  free( v );
+  
+  //testing::InitGoogleTest(&argc, argv);
+  //return RUN_ALL_TESTS();
+  
+  printf("done\n");
+  fflush(stdout); 
+  return 0;
+  
+}
