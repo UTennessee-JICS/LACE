f3998eb89d0354d48347cebf5faeb81ee7769184
Stephen Wood <swood19@utk.edu>
2017-09-25 09:28:28 -0400

    add comparison of solution vectors

--- results/post_process_orthog_compare_mgs_householder.m
+++ results/post_process_orthog_compare_mgs_householder.m
@@ -21,3 +21,24 @@ ylabel('Infinity Norm of Orthogonality Error in Krylov Search Space')
 title({'History of Krylov Search Space Orthogonality Error in'; 'FGMRES for one psuedo-time step of the 30p30n benchmark'})
 legend('FGMRES(MGS)+PariLU(1.5e+5)+MKLCSRTRSV','FGMRES(HouseHolder)+PariLU(1.5e+5)+MKLCSRTRSV',...    
     'Location','NorthWest')
+
+[x_mgs, rows, cols, entries] = mmread('../out_test_fgmres_10/30p30n_solution.mtx');
+[x_house, rows, cols, entries] = mmread('../out_test_fgmresh_10/30p30n_solution.mtx');                        
+
+
+discrepancy = norm(x_mgs - x_house)
+
+figure(3)
+plot(x_mgs)
+hold all
+plot(x_house)
+xlabel('Degrees of Freedom')
+ylabel('State values')
+title({'Solution vector found by FGMRES for one psuedo-time step of the 30p30n benchmark'})
+legend('FGMRES(MGS)+PariLU(1.5e+5)+MKLCSRTRSV','FGMRES(HouseHolder)+PariLU(1.5e+5)+MKLCSRTRSV')
+
+figure(4)
+plot(abs(x_mgs - x_house))
+xlabel('Degrees of Freedom')
+ylabel('State values abs(x_{MGS} - x_{Householder})')
+title({'Difference in solution vectors found by'; 'FGMRES for one psuedo-time step of the 30p30n benchmark'})
