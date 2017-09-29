902a98c4fa074909b0ef9ba71589b33a228a5b2e
Stephen Wood <swood19@utk.edu>
2017-09-29 10:25:36 -0400

    first parallelization of orthogonality error calculation

--- src/orthogonality_error.cpp
+++ src/orthogonality_error.cpp
@@ -1,13 +1,13 @@
 /*
     -- LACE (version 0.0) --
        Univ. of Tennessee, Knoxville
-       
+
        @author Stephen Wood
 
 */
 #include "../include/sparse.h"
 #include <mkl.h>
-#include <float.h> 
+#include <float.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -16,7 +16,7 @@
     -------
 
     Assess the orthogonality of the upper-left search x search portion of a matrix.
-    
+
 
     Arguments
     ---------
@@ -41,36 +41,41 @@
     ********************************************************************/
 
 
-extern "C" 
+extern "C"
 void
-data_orthogonality_error( data_d_matrix* krylov,  
+data_orthogonality_error( data_d_matrix* krylov,
   dataType* ortherr,
   int* imax,
-  int search ) 
+  int search )
 {
-  
+
   dataType zero = 0.0;
   dataType one = 1.0;
   dataType negone = -1.0;
-     
+
   data_d_matrix eye={Magma_DENSE};
   data_zvinit( &eye, search, search, zero );
+  #pragma omp parallel
+  #pragma omp for nowait
   for (int e=0; e<eye.ld; e++) {
-    eye.val[idx(e,e,eye.ld)] = 1.0; 
+    eye.val[idx(e,e,eye.ld)] = 1.0;
   }
-  
+
+
+  #pragma omp parallel
+  #pragma omp for nowait
   for (int i=0; i<eye.num_rows; i++) {
     for (int j=0; j<eye.num_cols; j++) {
       dataType sum = 0.0;
       for ( int k=0; k<krylov->num_rows; k++ ) {
         sum += krylov->val[idx(k,i,krylov->ld)] * krylov->val[idx(k,j,krylov->ld)];
       }
-      eye.val[idx(i,j,eye.ld)] -= sum;   
-    } 
+      eye.val[idx(i,j,eye.ld)] -= sum;
+    }
   }
-  
-  data_infinity_norm( &eye, imax, ortherr ); 
-  
+
+  data_infinity_norm( &eye, imax, ortherr );
+
   data_zmfree( &eye );
-  
+
 }
