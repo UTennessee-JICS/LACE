c7c3a19e79b4ddfcefa6171e63ac4b4468446ad6
Stephen Wood <swood19@utk.edu>
2017-11-09 16:13:53 -0500

    relocate EXPECT_ITERABLE_DOUBLE_EQ to container_tests.h

--- testing/test_matrix_operations.cpp
+++ testing/test_matrix_operations.cpp
@@ -7,6 +7,7 @@
 
 #include "mmio.h"
 #include "sparse_types.h"
+#include "container_tests.h"
 
 TEST(Operations_dense, dotproduct) {
   dataType dense_valcheck[35] = {
@@ -67,46 +68,6 @@ TEST(Operations_dense, dotproduct) {
   data_zmfree( &A );
   data_zmfree( &B );
 }
-
-//+++++++
-#define EXPECT_ITERABLE_DOUBLE_EQ( TYPE, ref, target) \
-{ \
-const TYPE& _ref(ref); \
-const TYPE& _target(target); \
-TYPE::const_iterator tarIter   = _target.begin(); \
-TYPE::const_iterator refIter = _ref.begin(); \
-unsigned int i = 0; \
-while(refIter != _ref.end()) { \
-    if ( tarIter == _target.end() ) { \
-        ADD_FAILURE() << #target \
-            " has a smaller length than " #ref ; \
-        break; \
-    } \
-    EXPECT_DOUBLE_EQ(* refIter, * tarIter) \
-        << "Vectors " #ref  " (refIter) " \
-           "and " #target " (tarIter) " \
-           "differ at index " << i; \
-    ++refIter; ++tarIter; ++i; \
-} \
-EXPECT_TRUE( tarIter == _target.end() ) \
-    << #ref " has a smaller length than " \
-       #target ; \
-}
-//+++++++
-
-//+++++++
-#define EXPECT_ARRAY_DOUBLE_EQ( length, ref, target) \
-{ \
-  unsigned int i = 0; \
-  for(i=0; i<length; i++) { \
-    EXPECT_DOUBLE_EQ(ref[i], target[i]) \
-    << "Arrays " #ref  " and " #target \
-           "differ at index " << i; \
-  } \
-}
-    //EXPECT_DOUBLE_EQ(ref[i], target[i])
-    // EXPECT_THAT( ref[i], testing::DoubleEq(target[i]) )
-//+++++++
 
 TEST(Operations_dense, dgemv) {
   dataType dense_valcheck[35] = {
