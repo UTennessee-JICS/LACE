698520c5b109b5e412c83a501263806a6721d7f2
Stephen Wood <swood@acf-login3.acf.nics.utk.edu>
2017-12-09 17:14:19 -0500

    run scripts from ACF

--- drivers/Solve_testMatrices.sh
+++ drivers/Solve_testMatrices.sh
@@ -7,7 +7,7 @@ MATRIXNAMES=(airfoil_2d DK01R GT01R olafu raefsky3 young3c dRdQ_sm)
 MATRIXPATHS=(../testing/matrices/airfoil_2d/ ../testing/matrices/DK01R/ ../testing/matrices/GT01R/ ../testing/matrices/olafu/ ../testing/matrices/raefsky3/ ../testing/matrices/young3c/ ../testing/matrices/)
 RHSNAMES=(ONES DK01R_b GT01R_b olafu_b raefsky3_b ONES ONES )
 RHSPATHS=(" " ../testing/matrices/DK01R/ ../testing/matrices/GT01R/ ../testing/matrices/olafu/ ../testing/matrices/raefsky3/ " " " ") 
-MACHINE=laptop
+MACHINE=KNL
 
 
 for ((i=0; i<${#MATRIXNAMES[@]}; i++)); do
