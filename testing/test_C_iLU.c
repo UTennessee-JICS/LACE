b6a4629e4c5489e88404d1e340a41208e44c813b
Stephen Wood <swood@utk.edu>
2018-01-22 11:53:56 -0500

    add C object compilation to enable OpenMP within exemplar C progam that uses the library.

--- testing/test_C_iLU.c
+++ testing/test_C_iLU.c
@@ -48,6 +48,48 @@ main(int argc, char * argv[])
     CHECK(data_z_csr_mtx((&A), matrix_name) );
 
     data_zprint_csr(A);
+
+    // ===================================================================
+    // MKL csrilu0  (Benchmark)
+    // ===================================================================
+    printf("%% MKL csrilu0  (Benchmark)\n");
+    data_d_matrix Amkl = { Magma_CSR };
+    data_zmconvert(A, &Amkl, Magma_CSR, Magma_CSR);
+
+    dataType wstart = omp_get_wtime();
+    CHECK(data_dcsrilu0_mkl(&Amkl) );
+    dataType wend = omp_get_wtime();
+    printf("%% MKL csrilu0 required %f wall clock seconds as measured by omp_get_wtime()\n", wend - wstart);
+
+    data_d_matrix Lmkl = {Magma_CSRL};
+    Lmkl.storage_type   = Magma_CSRL;
+    Lmkl.diagorder_type = Magma_UNITY;
+    data_zmconvert(Amkl, (&Lmkl), Magma_CSR, Magma_CSRL);
+    printf("test if Lmkl is lower: ");
+    data_zcheckupperlower(&Lmkl);
+    printf(" done.\n");
+
+    data_d_matrix Umkl = {Magma_CSRU};
+    Umkl.storage_type   = Magma_CSRU;
+    Umkl.diagorder_type = Magma_VALUE;
+    data_zmconvert(Amkl, (&Umkl), Magma_CSR, Magma_CSRU);
+    printf("test if Umkl is upper: ");
+    data_zcheckupperlower(&Umkl);
+    printf(" done.\n");
+    data_d_matrix LUmkl = { Magma_CSR };
+    data_zmconvert(Amkl, &LUmkl, Magma_CSR, Magma_CSR);
+
+    dataType Amklres       = 0.0;
+    dataType Amklnonlinres = 0.0;
+    // Check ||A-LU||_Frobenius for the whole and restricted to A's sparsity pattern
+    data_zilures( A, Lmkl, Umkl, (&LUmkl), (&Amklres), (&Amklnonlinres));
+    printf("MKL_csrilu0_res = %e\n", Amklres);
+    printf("MKL_csrilu0_nonlinres = %e\n", Amklnonlinres);
+    data_zmfree(&Amkl);
+    data_zmfree(&LUmkl);
+    fflush(stdout);
+
+
     data_zmfree(&A);
 
 }
