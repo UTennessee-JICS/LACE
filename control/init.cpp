1f9c0c746a5b356b296be053dc78eaa8140748fa
Chad Burdyshaw <chadburdyshaw@hotmail.com>
2018-07-10 11:07:01 -0400

    Changes to be committed:
    	modified:   control/init.cpp
    	Added an initializer for general sparse matrices that initializes block size to 1

--- control/init.cpp
+++ control/init.cpp
@@ -45,3 +45,38 @@ data_zvinit(
 
     return info;
 }
+
+
+extern "C" data_int_t
+data_zvinit_gen(data_d_matrix *x)
+{
+    data_int_t info = 0;
+
+    data_zmfree( x);
+
+    x->val = NULL;
+    x->diag = NULL;
+    x->row = NULL;
+    x->rowidx = NULL;
+    x->col = NULL;
+    x->blockinfo = NULL;
+    //x->storage_type = ;
+    x->sym = Magma_GENERAL;
+    x->diagorder_type = Magma_VALUE;
+    x->fill_mode = MagmaFull;
+    x->num_rows = 0;
+    x->num_cols = 0;
+    x->pad_rows = 0;
+    x->pad_cols = 0;
+    x->nnz = 0;
+    x->max_nnz_row = 0;
+    x->diameter = 0;
+    x->blocksize = 1;
+    x->numblocks = 1;
+    x->alignment = 1;
+    //x->major = ; // data_order_t major = MagmaColMajor is default see include/sparse.h
+    x->ld = 0;
+    x->ldblock=0;
+
+    return info;
+}
